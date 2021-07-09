af0b9491ab0e7cf3372a822e5632b734e1339bb3
Alquipo <alquiponeto@outlook.com.br>
2021-07-09 19:49:36 -0300

    feat: header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
-import {Container, Title} from './styles'
+import {Container, Header,UserWrapper, UserInfo,Photo,User,UserGreeting, UserName, Icon} from './styles'
 
 export function Dashboard(){
   return(
     <Container >
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{uri: 'https://avatars.githubusercontent.com/u/58523339?v=4'}} />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Alquipo</UserName>
+            </User>
+          </UserInfo>
+          <Icon name='power'/>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
