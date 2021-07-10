54c72a388ab9eb5e51f51a5e5449837edc13600a
Alquipo <alquiponeto@outlook.com.br>
2021-07-09 22:42:39 -0300

    feat: transaction card

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 
 import {
   Container, 
@@ -11,11 +13,54 @@ import {
   UserGreeting, 
   UserName, 
   Icon, 
-  HighlightCards} from './styles'
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionList
+} from './styles'
+
+export interface DataListProps extends TransactionCardProps {
+  id: string
+}
 
 export function Dashboard(){
+  const data: DataListProps[] = [
+  {
+    id: '1',
+    type: 'positive',
+    title: "Desenvolvimento de site",
+    amount: "R$ 12.000,00",
+    category: {
+      name: 'Vendas',
+      icon: 'dollar-sign'
+    },
+    date: "13/04/2020"
+  },
+  {
+    id: '2',
+    type: 'negative',
+    title: "Hamburgueria Pizzy",
+    amount: "R$ 59,00",
+    category: {
+      name: 'Alimentação',
+      icon: 'coffee'
+    },
+    date: "10/04/2020"
+  },
+  {
+    id: '3',
+    type: 'negative',
+    title: "Aluguel apartamento",
+    amount: "R$ 1.200,00",
+    category: {
+      name: 'Casa',
+      icon: 'shopping-bag'
+    },
+    date: "10/04/2020"
+  }
+]
   return(
-    <Container  >
+    <Container >
       <Header>
         <UserWrapper>
           <UserInfo>
@@ -49,6 +94,18 @@ export function Dashboard(){
           lastTransaction="01 à 16 de abril"
         />
       </HighlightCards>
+    
+    <Transactions>
+      <Title>Listagem</Title>
+
+      <TransactionList
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+          
+        />
+    </Transactions>
+    
     </Container>
   )
 }
